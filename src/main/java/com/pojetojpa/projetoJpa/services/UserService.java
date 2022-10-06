a36d206b209aeccaabbb59aa26755fd3fbe31402
Nicholas Luiz <nicholasnkzambom@gmail.com>
2022-10-06 13:43:10 -0300

    Service, registrando componente

---
+++ src/main/java/com/pojetojpa/projetoJpa/services/UserService.java
@@ -0,0 +1,27 @@
+package com.pojetojpa.projetoJpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pojetojpa.projetoJpa.entities.User;
+import com.pojetojpa.projetoJpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}

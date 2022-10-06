2bc36ed1c82d6760b769dda00b2234b13bd957f0
Nicholas Luiz <nicholasnkzambom@gmail.com>
2022-10-06 12:12:34 -0300

    Criando H2 database, profile test e JPA

--- src/main/java/com/pojetojpa/projetoJpa/resources/UserResource.java
+++ src/main/java/com/pojetojpa/projetoJpa/resources/UserResource.java
@@ -9,13 +9,11 @@ import com.pojetojpa.projetoJpa.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
-//função que retorna recursos da classe User
 public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "maria", "maria@gmail.com", "999999", "12345");
-		//retorna no corpo
 		return ResponseEntity.ok().body(u);
 	}
 
